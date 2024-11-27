ada2c436990e4060328c4b95ec7986d8b0a55729
Rolf Laich <rolf.laich@sensirion.com>
2024-11-27 21:32:17 +0100

    Update project configuration

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 ################################
 
 # Python versions this package is compatible with
-python_requires = '>=3.6, <4'
+python_requires = '>=3.8, <4'
 
 # Packages that this package imports. List everything apart from standard lib packages.
 install_requires = [
@@ -21,14 +21,17 @@ install_requires = [
 # Packages required for tests and docs
 extras_require = {
     'test': [
-        'flake8~=3.9.2',
-        'pytest~=5.4.3',
-        'pytest-cov~=2.12.1',
+        'flake8~=6.0.0',
+        'pytest~=8.1.1',
+        'pytest-cov~=6.0.0',
+        'mypy~=1.13.0',
     ],
     'docs': [
-        'docutils~=0.17.1',
-        'sphinx~=2.2.1',
-        'sphinx-rtd-theme~=0.4.3',
+        'lazy-object-proxy ~=1.7.1',
+        'sphinx~=7.1.0',
+        'docutils~=0.18.0',
+        'sphinx_rtd_theme~=1.3.0',
+        'sphinx-autoapi~=3.0.0'
     ]
 }
 
@@ -79,9 +82,8 @@ setup(
     extras_require=extras_require,
     classifiers=[
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
