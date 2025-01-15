c82c82387625a7fbafa70371895364064f08e569
Rolf Laich <rolf.laich@sensirion.com>
2025-01-15 12:21:26 +0100

    Fix mixin access
    In case a driver is derived the mixin access did not work anymore in the
    derived class.

--- tests/mixin_access_test.py
+++ tests/mixin_access_test.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import pytest
 
 from sensirion_driver_support_types.mixin_access import MixinAccess
 
@@ -48,8 +49,17 @@ class Bbase:
         return 'b_my_static_fun'
 
 
-class Single(Abase):
+class SimpleSingle(Abase):
+    """This is the way the current drivers are generated"""
     base = MixinAccess()
+
+    def __init__(self):
+        self.base.__init__('a_base')
+
+
+class Single(Abase):
+    """This is the way how drivers have to be generated such that thy can be used as base class"""
+    base = MixinAccess(Abase)
 
     def __init__(self):
         self.base.__init__('a_base')
@@ -68,27 +78,36 @@ class Multiple(Abase, Bbase):
         return f'{self.a_base.get_name()}\n{self.b_base.get_name()}'
 
 
-def test_single_base_access():
-    single = Single()
-    assert single.base.get_name() == 'a_base', "error calling member function with only one base class"
+class DeriveSingle(Single):
+    ...
 
 
-def test_single_static_base_access():
-    single = Single()
-    assert single.base.static_fun() == 'a_static_fun', "error calling static function with only one base class"
+class DeriveMultiple(Multiple):
+    ...
 
 
-def test_multiple_base_access():
-    cls = Multiple()
+@pytest.mark.parametrize("cls", [SimpleSingle(), Single(), DeriveSingle()])
+def test_single_base_access(cls):
+    assert cls.base.get_name() == 'a_base', "error calling member function with only one base class"
+
+
+@pytest.mark.parametrize("cls", [SimpleSingle(), Single(), DeriveSingle()])
+def test_single_static_base_access(cls):
+    assert cls.base.static_fun() == 'a_static_fun', "error calling static function with only one base class"
+
+
+@pytest.mark.parametrize("cls", [Multiple(), DeriveMultiple()])
+def test_multiple_base_access(cls):
     assert cls.a_base.get_name() == 'a_base', "error calling member function in base class 0"
     assert cls.b_base.get_name() == 'b_base', "error calling member function in base class 1"
 
 
-def test_multiple_class_base_access():
-    cls = Multiple()
+@pytest.mark.parametrize("cls", [Multiple(), DeriveMultiple()])
+def test_multiple_class_base_access(cls):
+    # cls = Multiple()
     assert cls.a_base.static_fun() == 'a_static_fun', "error calling static function on superclass 0"
     assert cls.b_base.class_fun() == 'b_class_fun', "error calling static function on superclass 1"
     assert cls.get_all_names() == 'a_base\nb_base', "error using selector within member function of derived class"
 
-    assert cls.b_base.my_static_fun() == 'b_my_static_fun', 'error calling static function without douplicate'
-    assert cls.a_base.my_private_name() == 'private_name:a_base', 'error calling member function without douplicate'
+    assert cls.b_base.my_static_fun() == 'b_my_static_fun', 'error calling static function without duplicate'
+    assert cls.a_base.my_private_name() == 'private_name:a_base', 'error calling member function without duplicate'
